4f3013421a10ff25a7ae49b50c7b981ee6f126f0
Jay Nelson <jay@duomark.com>
2014-10-14 17:25:04 -0700

    Labeled bs_serial messages so they aren't confused

--- src/elysium_bs_serial.erl
+++ src/elysium_bs_serial.erl
@@ -74,7 +74,7 @@ checkout_connection(Config) ->
     fetch_pid_from_queue(Queue_Name, Max_Retries, -1).
 
 -spec checkin_connection(config_type(), pid())
-                        -> {boolean(), {session_queue_name(), Idle_Count, Max_Count}}
+                        -> {boolean() | pending, {session_queue_name(), Idle_Count, Max_Count}}
                                when Idle_Count :: max_sessions() | ets_buffer:buffer_error(),
                                     Max_Count  :: max_sessions() | ets_buffer:buffer_error().
 %% @doc
@@ -89,9 +89,10 @@ checkout_connection(Config) ->
 %% @end
 checkin_connection(Config, Session_Id)
   when is_pid(Session_Id) ->
-    case is_process_alive(Session_Id) of
+    Pending_Queue = elysium_config:requests_queue_name(Config),
+    case is_process_alive(Session_Id) andalso ets_buffer:num_entries_dedicated(Pending_Queue) > 0 of
         false -> checkin_immediate (Config, Session_Id);
-        true  -> checkin_pending   (Config, Session_Id)
+        true  -> checkin_pending   (Config, Session_Id, Pending_Queue)
     end.
 
 -spec pend_request(config_type(), query_request()) -> any() | ets_buffer:buffer_error().
@@ -106,33 +107,8 @@ pend_request(Config, Query_Request) ->
     Start_Time    = os:timestamp(),
     Pending_Queue = elysium_config:requests_queue_name   (Config),
     Reply_Timeout = elysium_config:request_reply_timeout (Config),
-    wait_for_session(ets_buffer:write_dedicated(Pending_Queue, {{self(), Sid_Reply_Ref}, Start_Time}),
-                     Sid_Reply_Ref, Start_Time, Query_Request, Reply_Timeout).
-
-%% TODO: This receive loop needs to handle status queries and kill requests so it can be monitored.
-wait_for_session(_Pending_Request_Count, Sid_Reply_Ref, Start_Time, Query_Request, Reply_Timeout) ->
-    receive
-        {Sid_Reply_Ref, Session_Id} ->
-            case timer:now_diff(os:timestamp(), Start_Time) of
-                Expired when Expired > Reply_Timeout * 1000 ->
-                    {session_wait_timeout, Reply_Timeout};
-                Remaining_Time ->
-                    Self = self(),
-                    Worker_Reply_Ref = make_ref(),
-                    Worker_Fun = fun() -> exec_pending_request(Worker_Reply_Ref, Self,
-                                                               Session_Id, Query_Request) end,
-                    _ = spawn_monitor(Worker_Fun),
-                    Timeout_Remaining = Reply_Timeout - (Remaining_Time div 1000),
-                    receive_worker_reply(Worker_Reply_Ref, Timeout_Remaining)
-            end
-    after Reply_Timeout -> {wait_for_session_timeout, Reply_Timeout}
-    end.
-
-%% TODO: This receive loop needs to handle status queries and kill requests so it can be monitored.
-receive_worker_reply(Worker_Reply_Ref, Timeout_Remaining) ->
-    receive {Worker_Reply_Ref, Reply} -> Reply
-    after   Timeout_Remaining         -> {worker_reply_timeout, Timeout_Remaining}
-    end.
+    Pending_Count = ets_buffer:write_dedicated(Pending_Queue, {{self(), Sid_Reply_Ref}, Start_Time}),
+    wait_for_session(Pending_Count, Sid_Reply_Ref, Start_Time, Query_Request, Reply_Timeout).
 
 
 %%%-----------------------------------------------------------------------
@@ -181,6 +157,43 @@ fetch_pid_from_queue( Queue_Name, Max_Retries, Times_Tried) ->
         Error -> Error
     end.
 
+%% TODO: This receive loop needs to handle status queries and kill requests so it can be monitored.
+wait_for_session(_Pending_Request_Count, Sid_Reply_Ref, Start_Time, Query_Request, Reply_Timeout) ->
+    receive
+        {sid, Sid_Reply_Ref, Session_Id, Pending_Queue} ->
+            case is_process_alive(Session_Id) of
+
+                %% Requeue and wait for another Session_Id (this request is delayed and out of serial order)
+                false -> _ = ets_buffer:write_dedicated(Pending_Queue, {{self(), Sid_Reply_Ref}, Start_Time}),
+                         wait_for_session(_Pending_Request_Count, Sid_Reply_Ref, Start_Time, Query_Request, Reply_Timeout);
+
+                %% Handle the pending request if it is not expired already.
+                true  -> case timer:now_diff(os:timestamp(), Start_Time) of
+                             Expired when Expired > Reply_Timeout * 1000 ->
+                                 {session_wait_timeout, Reply_Timeout};
+
+                             %% Use only the Remaining time for the actual query
+                             Remaining_Time ->
+                                 Self = self(),
+                                 Worker_Reply_Ref = make_ref(),
+                                 Worker_Fun = fun() -> exec_pending_request(Worker_Reply_Ref, Self,
+                                                                            Session_Id, Query_Request) end,
+                                 _ = spawn_opt(Worker_Fun, [monitor]),
+                                 Timeout_Remaining = Reply_Timeout - (Remaining_Time div 1000),
+                                 receive_worker_reply(Worker_Reply_Ref, Timeout_Remaining)
+                         end
+            end;
+        Other -> error_logger:error_msg("Wait_for_session error: ~p", [Other])
+    after Reply_Timeout -> {wait_for_session_timeout, Reply_Timeout}
+    end.
+
+%% TODO: This receive loop needs to handle status queries and kill requests so it can be monitored.
+receive_worker_reply(Worker_Reply_Ref, Timeout_Remaining) ->
+    receive {wrr, Worker_Reply_Ref, Reply} -> Reply;
+            {'DOWN', _Ref, process, _Pid, Reason} -> error_logger:error_msg("Worker Reply DOWN: ~p", [Reason])
+    after   Timeout_Remaining -> {worker_reply_timeout, Timeout_Remaining}
+    end.
+
 -spec checkin_immediate(config_type(), pid())
                        -> {boolean(), {session_queue_name(), Idle_Count, Max_Count}}
                               when Idle_Count :: max_sessions() | ets_buffer:buffer_error(),
@@ -223,6 +236,12 @@ succ_checkin(Queue_Name, Max_Sessions, Session_Id) ->
     Available = ets_buffer:write_dedicated(Queue_Name, Session_Id),
     {true, report_available_resources(Queue_Name, Available, Max_Sessions)}.
 
+delay_checkin(Config) ->
+    Queue_Name   = elysium_config:session_queue_name (Config),
+    Max_Sessions = elysium_config:session_max_count  (Config),
+    Available    = ets_buffer:num_entries_dedicated  (Queue_Name),
+    {pending, report_available_resources(Queue_Name, Available, Max_Sessions)}.
+
 decay_causes_death(Config, _Session_Id) ->
     case elysium_config:decay_probability(Config) of
         Never_Decays when is_integer(Never_Decays), Never_Decays =:= 0 ->
@@ -232,23 +251,18 @@ decay_causes_death(Config, _Session_Id) ->
             R =< Probability
     end.
 
-checkin_pending(Config, Sid) ->
-    Pending_Queue = elysium_config:requests_queue_name(Config),
-    case ets_buffer:num_entries_dedicated(Pending_Queue) of
-          0 -> checkin_immediate(Config, Sid);
-        _N1 -> case ets_buffer:read_dedicated(Pending_Queue) of
-                  [] -> checkin_immediate(Config, Sid);
-                  [{{Pid, Sid_Reply_Ref}, When_Originally_Queued}] ->
-                      case timer:now_diff(os:timestamp(), When_Originally_Queued) of
-                          Expired when Expired < 0 -> checkin_pending(Config, Sid);
-                          _Remaining_Time ->
-                              Pid ! {Sid_Reply_Ref, Sid},
-                              case ets_buffer:num_entries_dedicated(Pending_Queue) of
-                                    0 -> checkin_immediate (Config, Sid);
-                                  _N2 -> checkin_pending   (Config, Sid)
-                              end
-                      end
-              end
+checkin_pending(Config, Sid, Pending_Queue) ->
+    case ets_buffer:read_dedicated(Pending_Queue) of
+        [] -> checkin_immediate(Config, Sid);
+        [{{Pid, Sid_Reply_Ref}, When_Originally_Queued}] ->
+            Reply_Timeout = elysium_config:request_reply_timeout(Config),
+            case timer:now_diff(os:timestamp(), When_Originally_Queued) of
+                Expired when Expired > Reply_Timeout * 1000 ->
+                    checkin_connection(Config, Sid);
+                _Remaining_Time ->
+                    Pid ! {sid, Sid_Reply_Ref, Sid, Pending_Queue},
+                    delay_checkin(Config)
+            end
     end.
 
 %% Watch Out! This function swaps from the Config on a checkin request to the
@@ -257,11 +271,13 @@ checkin_pending(Config, Sid) ->
 %% mixed up resulting in queries/updates/deletes talking to the wrong clusters.
 exec_pending_request(Reply_Ref, Reply_Pid, Sid, {bare_fun, Config, Query_Fun, Args, Consistency}) ->
     try   Reply = Query_Fun(Sid, Args, Consistency),
-          Reply_Pid ! {Reply_Ref, Reply}
-    after _ = checkin_immediate(Config, Sid)
+          Reply_Pid ! {wrr, Reply_Ref, Reply}
+    catch A:B -> error_logger:error_msg("Query execution caught ~p:~p for ~p ~p", [A,B, Reply_Pid, Args])
+    after _ = checkin_connection(Config, Sid)
     end;
 exec_pending_request(Reply_Ref, Reply_Pid, Sid, {mod_fun,  Config, Mod,  Fun, Args, Consistency}) ->
     try   Reply = Mod:Fun(Sid, Args, Consistency),
-          Reply_Pid ! {Reply_Ref, Reply}
-    after _ = checkin_immediate(Config, Sid)
+          Reply_Pid ! {wrr, Reply_Ref, Reply}
+    catch A:B -> error_logger:error_msg("Query execution caught ~p:~p for ~p ~p", [A,B, Reply_Pid, Args])
+    after _ = checkin_connection(Config, Sid)
     end.
